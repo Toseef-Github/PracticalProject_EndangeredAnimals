d2354e7357c864e4d6419890be789ba302d3e919
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-07-14 12:49:19 +0100

    created project

---
+++ src/main/java/com/qa/practicalprojectbackend/PracticalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qa.practicalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/practicalprojectbackend/PracticalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.practicalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
