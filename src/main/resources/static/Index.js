d5e67be1456a7257ab5f613753de54adfe5f8c5d
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-07-26 19:07:00 +0100

    added search functionality

--- src/main/resources/static/Index.js
+++ src/main/resources/static/Index.js
@@ -28,6 +28,64 @@ document.getElementById('animalsForm').addEventListener("submit", function (even
     console.log("animal: ", animal);
 });
 
+
+document.getElementById('animalsSearch').addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    axios.get(`http://localhost:8080/getAnimalByName/${form.searchName.value}`)
+        .then(res => {
+            console.log("RESPONSE: ", res);
+            form.searchName.focus();
+            form.reset();
+            console.log("success");
+            var existingSearchDiv = document.getElementById('searchAnimalsDiv');
+            if (existingSearchDiv != null) {
+                existingSearchDiv.remove();
+            }
+            for (let animal of res.data) {
+                const animalCol = document.createElement("div");
+                animalCol.id = "searchAnimalsDiv";
+                animalCol.className = "col-4";
+
+                const animalCard = document.createElement("div");
+                animalCard.className = "card";
+                animalCol.appendChild(animalCard);
+
+                const animalDiv = document.createElement("div");
+                animalDiv.className = "card-body";
+                animalCard.appendChild(animalDiv);
+
+                const animalName = document.createElement("h2");
+                animalName.innerText = animal.name;
+                animalDiv.appendChild(animalName);
+
+                const animalPopulation = document.createElement("p");
+                animalPopulation.innerText = "Current Population = " + animal.population;
+                animalDiv.appendChild(animalPopulation);
+
+                const animalLifespan = document.createElement("p");
+                animalLifespan.innerText = "Average Lifespan = " + animal.lifespan + " Years";
+                animalDiv.appendChild(animalLifespan);
+
+                const animalGroup = document.createElement("p");
+                animalGroup.innerText = "Group = " + animal.animalGroup;
+                animalDiv.appendChild(animalGroup);
+
+                const animalDiet = document.createElement("p");
+                animalDiet.innerText = "Diet = " + animal.diet;
+                animalDiv.appendChild(animalDiet);
+
+                output.appendChild(animalCol);
+            }
+        })
+        .catch(err => console.error(err));
+
+    
+});
+
+
 const output = document.getElementById('output');
 
 function renderAnimals() {
@@ -72,18 +130,12 @@ function renderAnimals() {
                 animalUpdate.classList.add("btn", "btn-secondary");
                 animalUpdate.addEventListener("click", () => {
                     if (confirm('Are you sure?')) {
-                        txt = console.log(updateAnimal(animal.id));
+                            console.log(updateAnimal(animal.id));
                     } else {
-                    txt = "You pressed cancel";
+                    console.log("You pressed cancel");
                     }
                 });
                 
-                
-                
-                // addEventListener("click", () => {
-                //     console.log();
-                //     updateAnimal(animal.id);
-                // });
 
                 animalDiv.appendChild(animalUpdate);
 
@@ -95,9 +147,9 @@ function renderAnimals() {
                 animalDelete.addEventListener("click", () => {
 
                     if (confirm('Are you sure?')) {
-                        txt = console.log(deleteAnimal(animal.id));
+                            console.log(deleteAnimal(animal.id));
                     } else {
-                    txt = "You pressed cancel";
+                    console.log("You pressed cancel");
                     }
                 });
 
